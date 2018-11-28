b6621112b76bb400e8abb1cc38407264f1ddf658
wangqiangqiang <764372409@qq.com>
2018-11-28 11:33:33 +0800

    框架搭建

---
+++ src/test/java/com/yuanmaxinxi/VoteSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuanmaxinxi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VoteSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
