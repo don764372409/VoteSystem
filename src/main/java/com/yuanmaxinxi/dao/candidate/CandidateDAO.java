97ce26758190b8e81c4de54df4a8cd5de0afc6a4
Liudan <1070106148@qq.com>
2018-11-28 17:43:43 +0800

    更新

--- src/main/java/com/yuanmaxinxi/dao/candidate/CandidateDAO.java
+++ src/main/java/com/yuanmaxinxi/dao/candidate/CandidateDAO.java
@@ -1,11 +1,11 @@
-package com.yuanmaxinxi.dao;
+package com.yuanmaxinxi.dao.candidate;
 import com.yuanmaxinxi.domain.Candidate;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 @Mapper
 @Repository
-public interface CandidateMapper{
+public interface CandidateDAO{
 	int insert(Candidate obj);
 
 	int update(Candidate obj);
