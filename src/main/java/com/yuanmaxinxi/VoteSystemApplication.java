b9c6a42b8636ab9882fab3dcfb12d6002cb8dc6b
wangqiangqiang <764372409@qq.com>
2018-11-28 18:08:23 +0800

    登录控制器

--- src/main/java/com/yuanmaxinxi/VoteSystemApplication.java
+++ src/main/java/com/yuanmaxinxi/VoteSystemApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class VoteSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VoteSystemApplication.class, args);
 	}
