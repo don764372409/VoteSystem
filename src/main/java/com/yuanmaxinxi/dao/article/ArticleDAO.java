973e12a08b3611433b84b07303fee650a55a972b
admin <admin@DESKTOP-H1D5VEE>
2018-11-28 18:45:41 +0800

    dk

--- src/main/java/com/yuanmaxinxi/dao/article/ArticleDAO.java
+++ src/main/java/com/yuanmaxinxi/dao/article/ArticleDAO.java
@@ -7,13 +7,9 @@ import java.util.List;
 @Repository
 public interface ArticleDAO{
 	int insert(Article obj);
-
 	int update(Article obj);
-
 	int delete(Long id);
-
 	Article selectOneById(Long id);
-
 	List<Article> selectAll();
 
 }
